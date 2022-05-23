2eb44f1aea49757944aa01d30c22d81ae6db88b0
Manan Shah <shahmanan31@gmail.com>
2022-05-23 12:36:57 -0400

    Create theme.js

---
+++ assets/theme.js
@@ -0,0 +1,20 @@
+$('document').ready(function() {
+  // Back to top
+  var backTop = $(".back-to-top");
+
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
